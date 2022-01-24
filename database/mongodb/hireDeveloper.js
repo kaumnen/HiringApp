65bce6ef6eb8d8802af646b19b7e590c5457c408
kaumnen <main@komnen.xyz>
2022-01-24 21:48:50 +0100

    add new fields to update query

--- database/mongodb/hireDeveloper.js
+++ database/mongodb/hireDeveloper.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 import { connect } from './connect.js';
 
-export function hireDeveloper(id) {
+export function hireDeveloper(id, typeOfHire, startDate, endDate) {
     return new Promise((resolve, reject) => {
         connect().then(() => {
-
             mongoose.model('Developer').updateOne({ _id: id }, { 
-                hired: true
+                hired: true,
+                typeOfHire: typeOfHire,
+                startDate: startDate,
+                endDate: endDate
                 }, (error) => {
                 if (error) {
                     reject(error);
